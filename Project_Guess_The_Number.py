ae8162356e3851462de74ea17485e23a9bba1eb1
VanessaH <vanessa50327@gmail.com>
2022-06-09 21:31:21 +0800

    initial document of python project

---
+++ Project_Guess_The_Number.py
@@ -0,0 +1,51 @@
+#Number Guessing Game Objectives:
+# Include an ASCII art logo. V
+# Allow the player to submit a guess for a number between 1 and 100. V
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
+import random
+
+logo="""
+  ________                            .__                   ________                       
+ /  _____/ __ __   ____   ______ _____|__| ____    ____    /  _____/_____    _____   ____  
+/   \  ___|  |  \_/ __ \ /  ___//  ___/  |/    \  / ___\  /   \  ___\__  \  /     \_/ __ \ 
+\    \_\  \  |  /\  ___/ \___ \ \___ \|  |   |  \/ /_/  > \    \_\  \/ __ \|  Y Y  \  ___/ 
+ \______  /____/  \___  >____  >____  >__|___|  /\___  /   \______  (____  /__|_|  /\___  >
+        \/            \/     \/     \/        \//_____/           \/     \/      \/     \/ 
+"""
+print(logo)
+print("Welcome to the Number Guessing Game!")
+
+print("I'm thinking about a number between 1 to 100.")
+random_num = random.randint(1,100)
+print(random_num)
+
+diff = input("Choose a difficulty! Type 'easy' or 'hard': ")
+attempts=0
+used_attempts=1
+
+if diff=="easy":
+  attempts=10
+elif diff=="hard":
+  attempts=5
+
+while attempts!=0:
+  guess = int(input("Make a guess: "))
+  if guess!= random_num:
+    if guess<random_num:
+      print("Too low.")
+    elif guess>random_num:
+      print("Too high.")
+    attempts-=1
+    used_attempts+=1
+    print(f"Guess again. You have {attempts} attempts left.")
+  elif guess==random_num:
+    print(f"You've made the right guess! It takes you only {used_attempts} attempts!")
+    break
+
+if attempts==0:
+  print("You have run out of guesses. Game over QQ.")
---
+++ Project_Rock_Paper_Scissors.py
@@ -0,0 +1,52 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+computer_choice=random.randint(0,2)
+if (choice==0):
+  print(rock)
+elif (choice==1):
+  print(paper)
+elif (choice==2):
+  print(scissors)
+
+if (computer_choice==0):
+  print(rock)
+elif (computer_choice==1):
+  print(paper)
+elif (computer_choice==2):
+  print(scissors)
+
+if (choice==computer_choice):
+  print("This is a draw!")
+elif (choice==0 and computer_choice==1) or (choice==1 and computer_choice==2) or (choice==2 and computer_choice==0):
+  print("Human loses.")
+else:
+  print("Human wins.")
+
